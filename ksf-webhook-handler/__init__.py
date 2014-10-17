a85d32524ed4d1c32de77b342131854b56acc65c
Richard Weber <richard.weber@ksfmedia.fi>
2014-10-17 13:23:47 +0300

    Path as argument

--- ksf-webhook-handler/__init__.py
+++ ksf-webhook-handler/__init__.py
@@ -2,12 +2,17 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import argparse
 import pprint
+import os
+
 
 class GitHookHandler(BaseHTTPRequestHandler):
 
+    def set_path(self, path):
+        self.project_path = path
+
     def handle_payload(self, json_payload):
-        print("JSON payload")
         pprint.pprint(json_payload)
+
 
     def do_POST(self):
         data_length = int(self.headers['Content-Length'])
@@ -18,9 +23,13 @@ class GitHookHandler(BaseHTTPRequestHandler):
         self.send_response(200)
 
 
-argparser = argparse.ArgumentParser(description="Git hook handler")
-argparser.add_argument('port', type=int, help="TCP port to listen on", default=8000)
+argparser = argparse.ArgumentParser(description="Git hook handler.")
+argparser.add_argument('port', type=int, help="TCP port to listen on.")
+argparser.add_argument('path', type=str, help="Path to projects root.")
 args = argparser.parse_args()
 
-server = HTTPServer(("", args.port), GitHookHandler)
+handler = GitHookHandler()
+handler.set_path(args.path)
+
+server = HTTPServer(("", args.port), handler)
 server.serve_forever()
