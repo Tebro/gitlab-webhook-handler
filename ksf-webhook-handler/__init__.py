6440e2eec3b49e03deb1933339355767933f2822
Richard Weber <richard.weber@ksfmedia.fi>
2014-10-17 13:42:05 +0300

    Fix for for path setting.

--- ksf-webhook-handler/__init__.py
+++ ksf-webhook-handler/__init__.py
@@ -5,11 +5,9 @@ import pprint
 import os
 from subprocess import call
 
+path = ""
 
 class GitHookHandler(BaseHTTPRequestHandler):
-
-    def set_path(self, path):
-        self.projects_path = path
 
     def handle_payload(self, json_payload):
         #pprint.pprint(json_payload)
@@ -19,11 +17,10 @@ class GitHookHandler(BaseHTTPRequestHandler):
         repo = json_payload['repository']['name']
         url = json_payload['repository']['url']
 
-        if os.path.isdir("%s%s" % (self.projects_path, repo)):
-            call("git --git-dir=%s%s/.git pull origin master" % (self.projects_path, repo), shell=True)
+        if os.path.isdir("%s%s" % (path, repo)):
+            call("git --git-dir=%s%s/.git pull origin master" % (path, repo), shell=True)
         else:
-            call("git clone %s %s%s" % (url, self.projects_path, repo), shell=True)
-
+            call("git clone %s %s%s" % (url, path, repo), shell=True)
 
 
     def do_POST(self):
@@ -40,13 +37,10 @@ argparser.add_argument('port', type=int, help="TCP port to listen on.")
 argparser.add_argument('path', type=str, help="Path to projects root.")
 args = argparser.parse_args()
 
-handler = GitHookHandler()
 
 path = args.path
 if not path.endswith('/'):
     path += "/"
 
-handler.set_path(path)
-
-server = HTTPServer(("", args.port), handler)
+server = HTTPServer(("", args.port), GitHookHandler)
 server.serve_forever()
